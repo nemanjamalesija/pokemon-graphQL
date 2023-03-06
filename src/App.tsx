aa9d03907504db69439fa89e581df6c269d55364
Nemanja Malesija <malesija13@gmail.com>
2023-03-06 23:41:11 +0100

    Make test query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useQuery, gql } from '@apollo/client';
+
+const TEST_QUERY = gql`
+  query {
+    pokemon_v2_itemname {
+      item_id
+      name
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(TEST_QUERY);
+
+  if (loading) return <h1>Loading...</h1>;
+  console.log(data);
+
   return <div className="App"></div>;
 }
 
